b85ff88fae8f5dc7743e3debd261bc16682f3906
Kade Griffith <kadeg95@gmail.com>
2021-09-30 11:46:40 -0600

    added npm react

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
+const path = require('path');
 
-console.log('Hello, World!!');
 console.log(process.argv[2]);
 console.log(process.env.USERNAME);
-console.log(process.env.FOO);
+
 
 const express = require('express');
 const server = express();
 server.use(express.json());
+server.use(express.static(
+    path.join(__dirname, 'client/build')
+));
 
-
-
-server.get('/', (req, res) => {
-    res.send('<h1>Welcome!</h1>');
-});
 
 server.get('/hello', (req, res) => {
     res.send('<h1>HELLO THERE!</h1>');
+});
+
+server.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, 'client/build', 'index.html')
+    );
 });
 
 const port = process.env.PORT || 3000;
